f75b7d94041baf7bef9ff988b9ae2e5ed38d5cd9
Mitru Carmen <carmen.mitru@gmail.com>
2017-06-04 21:23:21 +0300

    Update solution
    I updated the solution :
    -create a class called Node and one called Linked List 
    -fix the function kth_to_last_node()

--- nThElemLinkedList.py
+++ nThElemLinkedList.py
@@ -1,48 +1,68 @@
 """
 Implement an algorithm to find the kth to last element of a singly linked list.
-
-The first approach is to loop through the list so we could get the lenght of the linked list ,N.
-Next we can see the element K th to the last element using formula : ( length(list)- k + 1 )
-
-Complexity :
-Time complexicity: O(k)
-Space complexicity: O(1)
-
 """
-class Node():
-    def __init__(self, val):
-        self.next = None
-        self.value = val
-    
-    def next(self):
-        return self.next
-    
-    def setNext(self, nextNode):
-        self.next = nextNode
-        
+class Node(object):
+    # Function to initialize the node object
+    def __init__(self, data=None, next_node=None):
+        self.data = data  # Assign data
+        self.next_node = next_node  # Initialize 
+                          # next as null
+    def get_data(self):
+        return self.data
 
-def findElement(head, k):
-    if head == None:
-        return None
-    
-    #get the length of the list 
-    currentNode = head
-    num = 1  
-    while currenNode.next:
-        num += 1
-        currentNode = currentNode.next
-    
-    #Special cases when 
-    if num < N or N <= 0: 
-        return None
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+
+class LinkedList(object):
+    # Function to initialize the Linked 
+    # List object
+    def __init__(self, head=None):
+        self.head = head
+
+    def insert(self, data):
+        # 1 & 2: Allocate the Node &
+        #Put in the data
+        new_node = Node(data)
+        # 3. Make next of new Node as head
+        new_node.set_next(self.head)
+        # 4. Move the head to point to new Node 
+        self.head = new_node
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.get_next()
+        return count
+
+      
+    def kth_to_last_node(self,k):
+        if k < 1:
+            raise ValueError("Impossible to find less than first to last node")
+        current = self.head
+        count = 0
+        s = self.size()
         
-    if N <= 0: 
-        return None
-    
-    # get num - k + 1 node
-    currentNode = head
-    currentNum  = 1
-    while currentNum < num - k + 1:
-        currentNum += 1
-        currentNode = currentNode.next
-    return currentNode.value
+        if k > s:
+            raise ValueError("K is larger than the length of the linked list")
+   
+        while current is not None and count < s - k:
+            count += 1
+            current = current.get_next()
+        print current.get_data()
+            
+       
+         
+         
+
+temp = LinkedList()
+temp.insert(3)
+temp.insert(4)
+temp.insert(5)
+temp.insert(6)
+temp.kth_to_last_node(1)
